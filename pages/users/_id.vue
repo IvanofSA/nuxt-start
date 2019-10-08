3321a3ba34287d61a1534a52e271672a106b7aca
SIvano03 <Sergey.Ivanov@isobar.ru>
2019-10-08 19:02:48 +0300

    page users, user

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,17 +1,21 @@
 <template>
 	<section>
-		<h1>User {{ $route.params.id }}</h1>
+		<h1>{{user.name}}</h1>
+
+		<hr>
+
+		<h3>{{user.email}}</h3>
 	</section>
 </template>
 
 <script>
 	export default {
 		validate({params}) {
-			return /^\d+$/.test(params.id);
+			return /^\d+$/.test(params.id)
+		},
+		async asyncData({$axios, params}) {
+			const user = await $axios.$get('https://jsonplaceholder.typicode.com/users/' + params.id)
+			return {user}
 		}
 	}
 </script>
-
-<style scoped>
-
-</style>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<section>
-		<h1>Users Page</h1>
+		<h1>{{pageTitle}}</h1>
 		<ul>
-			<li v-for="(user) in 5" :key="user">
-				<a href="" @click.prevent="openUser(user)">User {{user}}</a>
+			<li v-for="user in users" :key="user.id">
+				<a href="#" @click.prevent="openUser(user)">{{user.name}}</a>
 			</li>
 		</ul>
 	</section>
@@ -11,10 +11,19 @@
 
 <script>
 	export default {
+		async asyncData({$axios}) {
+			let users = await $axios.$get('https://jsonplaceholder.typicode.com/users')
+			return {users}
+		},
 		name: "index",
+		data() {
+			return {
+				pageTitle: 'User Page'
+			}
+		},
 		methods: {
 			openUser(user) {
-				this.$router.push('/users/' + user)
+				this.$router.push('/users/' + user.id)
 			}
 		}
 	}
