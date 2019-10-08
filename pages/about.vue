61d482823111202a6f4f803dee6ef487fa83a8f8
SIvano03 <Sergey.Ivanov@isobar.ru>
2019-10-08 18:43:10 +0300

    about,index,login page + navbar

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<h1> About page</h1>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur culpa esse provident quaerat. Aliquid amet doloribus ducimus perferendis similique!</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id incidunt, iste laborum libero provident rem.</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet deserunt est et illum libero nam numquam sequi. Accusantium dolor, doloremque exercitationem nemo quae repudiandae?</p>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "aboyt"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<section>
+		<h1>Users Page</h1>
+		<ul>
+			<li v-for="(user) in 5" :key="user">
+				<a href="" @click.prevent="openUser(user)">User {{user}}</a>
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "index",
+		methods: {
+			openUser(user) {
+				this.$router.push('/users/' + user)
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
